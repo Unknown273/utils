207e4c1f5549b34557c95a414d1edc9c85d0c409
Kazi Yusuf <71485724+Unknown273@users.noreply.github.com>
2023-03-26 17:40:51 +0000

    Replace double quotes with single quotes

--- remove-versions.py
+++ remove-versions.py
@@ -1,15 +1,15 @@
 try:
-  file_contents = ""
-  with open("requirements.txt", "r") as myFile:
+  file_contents = ''
+  with open('requirements.txt', 'r') as myFile:
     pkgs = myFile.read()
     pkgs = pkgs.splitlines()
 
   for pkg in pkgs:
-      file_contents += pkg.split('==')[0] + "\n"
+      file_contents += pkg.split('==')[0] + '\n'
 
-  with open("requirements.txt", "w") as myFile:
+  with open('requirements.txt', 'w') as myFile:
     myFile.write(file_contents)
-    print("Successfully generated the new requirements.txt file")
+    print('Successfully generated the new requirements.txt file')
 
 except FileNotFoundError:
   print(
